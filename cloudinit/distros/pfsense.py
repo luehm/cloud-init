2e5682c88ee5d8835059a5aca34dba39451d3292
Alex Luehm <alex.luehm@ultimateknowledge.com>
2025-01-28 18:38:08 -0600

    rework - supports user and group creation

--- cloudinit/distros/pfsense.py
+++ cloudinit/distros/pfsense.py
@@ -4,15 +4,14 @@
 #
 # This file is part of cloud-init. See LICENSE file for license information.
 
+import bcrypt
 import logging
-import os
-import re
-import xml.etree.ElementTree as ET
-from io import StringIO
 
-import cloudinit.distros.bsd
+import cloudinit.distros.freebsd
 from cloudinit import subp, util
-from cloudinit.distros.networking import FreeBSDNetworking
+import cloudinit.distros.pfsense_utils as pf_utils
+
+#from cloudinit.distros.networking import FreeBSDNetworking
 from cloudinit.settings import PER_INSTANCE
 
 LOG = logging.getLogger(__name__)
@@ -23,136 +22,223 @@ class Distro(cloudinit.distros.freebsd.Distro):
     Distro subclass for pfSense
     """
 
-    pf_config_path = "/cf/conf/config.xml"
+    user_node = "./system/user"
+    group_node = "./system/group"
+    next_uid_node = "./system/nextuid"
+    next_gid_node = "./system/nextgid"
 
-    group_add_cmd_prefix = None # groups are maintained within config.xml
-
-    @classmethod
-    def set_config_object(self, key, value, node=None):
-        """
-        For the given xml path, create an element with the specified properties.
-        """
-
-        # Find parent element in document
-        tree = ET.parse(self.pf_config_path)
-        root = tree.getroot()
-        node = root.find(key)
-        if node is None:
-            raise ValueError("No such key: %s" % key)
-
-        # Create new element 
-        element = ET.Element(key)
-
-        # Create element attributes, if provided
-        for k, v in value.items():
-            element.set(k, v)
-
-        # Append element to parent node
-        node.append(element)
-
-        # Write changes to file
-        tree.write(self.pf_config_path)
-
-    @classmethod
-    def set_config_value(self, key, value):
-        """
-        For the givem xml path, set the value of the specified element
-        """
-
-        # Find parent element in document
-        tree = ET.parse(self.pf_config_path)
-        root = tree.getroot()
-        node = root.find(key)
-        if node is None:
-            raise ValueError("No such key: %s" % key)
-
-        # Set element value
-        node.text = value
-
-        # Write changes to file
-        tree.write(self.pf_config_path)
-
-    @classmethod
-    def get_config_object(self, key):
-        """
-        For the given xml path in key, get the element
-        """
-
-        # Find element in document
-        tree = ET.parse(self.pf_config_path)
-        root = tree.getroot()
-        node = root.find(key)
-        if node is None:
-            raise ValueError("No such key: %s" % key)
-
-        return node
-
-    @classmethod
-    def get_config_value(self, key):
-        """
-        For the given xml path in key, get the value
-        """
-
-        # Find element in document
-        tree = ET.parse(self.pf_config_path)
-        root = tree.getroot()
-        node = root.find(key)
-        if node is None:
-            raise ValueError("No such key: %s" % key)
-
-        return node.text
-
-    @classmethod
-    def reload_pf(cls, rcs=None):
+    def _reload_pf(self, cls, rcs=None):
         """
         Tell pf to reload its configuration
         """
         return subp.subp(["/etc/rc.reload_all"], capture=True, rcs=rcs)
 
     def create_group(self, name, members=None):
+        """
+        Create a new group
+        Groups are stored within the pfsense/system element as a new group entry
+        """
+
+        # Check if group already exists
+        groups = pf_utils.get_config_element(Distro.group_node)
+        if [g for g in groups if g["name"] == name]:
+            LOG.info("Group %s already exists, skipping.", name)
+            return False
+
+        # Check if name is empty
+        if name == None or name == "":
+            LOG.info("Unable to create group, name cannot be empty")
+            return False
+
+        # Get next available gid
+        gid = pf_utils.get_config_value(Distro.next_gid_node)
+
+        # Create new group
+        group = {}
+        group["name"] = name
+        group["gid"] = gid
+        group["scope"] = "system"
+
+        # Increment next gid tracker
+        pf_utils.set_config_value(Distro.next_gid_node, str(int(gid) + 1))
+
+        if not members:
+            members = []
+        elif not isinstance(members, list):
+            members = [members]
+
+        # Add group members which currently exist on system
+        group["gid"] = []
+        existing_users = pf_utils.get_config_element(Distro.user_node)
+        for member in members:
+            if not [u for u in existing_users if u["name"] == member]:
+                LOG.warning("Unable to add group member '%s' to group '%s'; user does not exist.", member, name)
+                continue
+
+            group["gid"].append(member["uid"])
+
+        # Add group to system element
+        pf_utils.append_config_element(Distro.group_node, group)
+
+    def _add_user_to_group(self, user_id, group_name):
+        """
+        Add a user to a group
+        """
+
+        # Check if group exists
+        groups = pf_utils.get_config_element(Distro.group_node)
+        group = None
+        for g in groups:
+            if g["name"] == group_name:
+                group = g
+                break
+
+        # If group does not exist, return
+        if group is None:
+            return False
+        
+        # If user is already in group, return
+        if user_id in group["member"]:
+            LOG.info("User %s already in group %s", user_id, group_name)
+            return True
+
+        # Add user to group
+        group["member"].append(user_id)
+        pf_utils.replace_config_element(Distro.group_node, "name", group_name, group)
+        return True
+
+        
+    def _add_ssh_key(self, user, key):
+        raise NotImplementedError()
+
+    def set_passwd(self, user, passwd, hashed=False):
+        """
+        Set the password for a user
+        """
+
+        # Check if user exists
+        users = pf_utils.get_config_element(Distro.user_node)
+        n = None
+        for u in users:
+            if u["name"] == user:
+                n = u
+                break
+        if n is None:
+            LOG.info("User %s does not exist", user)
+            return False
+
+        # Set password
+        if hashed:
+            # Check if password is bcrypt format
+            if passwd.startswith("$2a$"):
+                n["bcrypt-hash"] = passwd
+                pf_utils.set_config_value(Distro.user_node + "/bcrypt-hash", passwd)
+            else:
+                print("Invalid bcrypt hash: %s", passwd)
+                LOG.info("Invalid bcrypt hash for user %s, skipping", user)
+                return False
+        else:
+            # Generate bcrypt hash of user password
+            n["bcrypt-hash"] = bcrypt.hashpw(passwd.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+
+            pf_utils.replace_config_element(Distro.user_node, "name", user, n)
+
+        return True
+
+    def lock_passwd(self, user):
+        """
+        Lock a user's password, effectively disabling the account.
+        Implemented on pfSense by the <disabled> element in the user's configuration
+        """
+
+        # Check if user exists
+        users = pf_utils.get_config_element(Distro.user_node)
+        n = None
+        for u in users:
+            if u["name"] == user:
+                n = u
+                break
+        if n is None:
+            LOG.info("User %s does not exist", user)
+            return False
+        
+        # Lock user password
+        n["disabled"] = None
+        pf_utils.replace_config_element(Distro.user_node, "name", user, n)
+        return True
+
+    def chpasswd(self, user, passwd):
         raise NotImplementedError()
 
     def add_user(self, name, **kwargs):
         """
         Add a user to the system
         Users are stored within the pfsense/system element as a new user entry
+
+        Supported user properties:
+        - gecos: User description
+        - lock_passwd: Lock user password
+        - expiredate: User expiration date
+        - groups: List of groups to add user to
+        - name: User name
+        - groups[list]: List of groups to add user to
+        - ssh_authorized_keys
+        - plain_text_passwd
+        - hashed_passwd
+        - passwd
+
         """
 
-        user_parent_node = "pfsense/system"
-        
         # Check if user already exists
-        if util.is_user(name):
+        users = pf_utils.get_config_element(Distro.user_node)
+        if [u for u in users if u["name"] == name]:
             LOG.info("User %s already exists, skipping.", name)
             return False
 
-        # Create new user element
-        user = ET.Element("user")
+        if name == None or name == "":
+            LOG.info("Unable to create user, name cannot be empty")
+            return False
+        
+        uid = pf_utils.get_config_value(Distro.next_uid_node)
 
-        # Set user attributes
-        user.set("name", name)
+        # Create new user
+        user = {}
+        user["name"] = name
+        user["uid"] = uid
+
+        # Increment next uid tracker
+        pf_utils.set_config_value(Distro.next_uid_node, str(int(uid) + 1))
+
+        supported_user_passwd_formats = {
+            "plain_text_passwd": False,
+            "hashed_passwd": True,
+            "passwd": True
+        }
+
         for key, val in kwargs.items():
-            user.set(key, val)
+            if key == "gecos":
+                user["descr"] = val
+            elif key == "expiredate":
+                user["expires"] = val
+            elif key == "ssh_authorized_keys":
+                for key in val:
+                    self._add_ssh_key(name, key)
 
         # Add user to system element
-        self.set_config_object("system", user)
+        pf_utils.append_config_element(Distro.user_node, user)
 
-        # Reload pf
-        self.reload_pf()
+        # Parse remaining user properties
+        for key, val in kwargs.items():
+            if [k for k in supported_user_passwd_formats.keys() if k == key]:
+                self.set_passwd(name, val, supported_user_passwd_formats[key])
+            elif key == "groups":
+                if not isinstance(val, list):
+                    val = [val]
+                for group in val:
+                    if not self._add_user_to_group(uid, group):
+                        LOG.warning("Unable to add user '%s' to group '%s - group does not exist'", name, group)
+
+            elif key == "expired":
+                self.lock_passwd(name)
 
         return True
-    
-    def expire_passwd(self, user):
-        raise NotImplementedError()
-    
-    def set_passwd(self, user, passwd):
-        raise NotImplementedError()
-    
-    def chpasswd(self, user, passwd):
-        raise NotImplementedError()
-    
-    def lock_passwd(self, user):
-        raise NotImplementedError()
-    
-    def setup_user_keys(self, user, keys):
-        raise NotImplementedError()
-    
